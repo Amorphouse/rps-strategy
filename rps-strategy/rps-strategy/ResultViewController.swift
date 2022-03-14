0f2e4b9a6ba20cadd241dc1f0d811866953a3c73
yusuke suzuki <92585953+Amorphouse@users.noreply.github.com>
2022-03-15 00:26:03 +0900

    asset追加 TOP画面適当に

---
+++ rps-strategy/rps-strategy/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  rps-strategy
+//
+//  Created by 鈴木悠介 on 2022/03/15.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
